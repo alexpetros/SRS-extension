841e0001a44af1a493132e7b14ce64170310d501
Alexander Petros <apetros15@gmail.com>
2018-10-20 22:38:49 -0400

    styles load but not images

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+// set up from chrome extensions with a little help from this gist:
+// https://gist.github.com/ayastreb/8f094c7ea17eb36cb1e6b5b9db9042c0
+
+// set to 'production' or 'development' in your env
+const env = process.env.NODE_ENV || 'development'
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const finalCSSLoader = (env === 'production') ? MiniCssExtractPlugin.loader : { loader: 'style-loader' };
+
+
+module.exports = {
+  mode: env,
+  entry: ['./src'], // this is where our app lives
+  devtool: 'source-map', // this enables debugging with source in chrome devtools
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s?css/,
+        use: [
+          finalCSSLoader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              useRelativePath: true,
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new CopyWebpackPlugin([
+      { from: './src/manifest.json'},
+    ])
+  ],
+};
+
