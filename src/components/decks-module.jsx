243caca75e39c687ef3d89a457b7b859893e583a
Alexander Petros <apetros15@gmail.com>
2019-05-28 12:00:54 -0400

    basic confirmation of delete (no api)

--- src/components/decks-module.jsx
+++ src/components/decks-module.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMinus, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 import { getUser } from '../api'
 
 const DeckRow = (props) => {
-  const { deckName } = props
+  const { deckName, isConfirming, onDeleteClick } = props
 
-  return <div>{deckName}</div>
+  return (
+    <div className="deck-row">
+      <div>{deckName}</div>
+      <FontAwesomeIcon
+        icon={isConfirming ? faTrash : faMinus}
+        onClick={() => { onDeleteClick(deckName) }} />
+    </div>
+  )
 }
 
 export default class decksModule extends Component {
@@ -13,25 +22,49 @@ export default class decksModule extends Component {
     super(props)
 
     this.state = {
-      // confirmDelete: null,
+      confirmDelete: null,
       decks: [],
     }
+
+    this.onDeleteClick = this.onDeleteClick.bind(this)
   }
 
   componentDidMount() {
     const { username } = this.props
 
+    // get the user's profile in order to display decks
     getUser(username).then((user) => {
       this.setState({ decks: user.decks })
     })
   }
 
+  onDeleteClick(deckName) {
+    const { confirmDelete } = this.state
+
+    if (confirmDelete) {
+      // delete item and reset confirmation
+      this.setState({ confirmDelete: null })
+    } else {
+      this.setState({ confirmDelete: deckName })
+    }
+  }
+
   render() {
-    const { decks } = this.state
+    const { decks, confirmDelete } = this.state
 
     return (
-      <div>
-        {decks.map(deck => <DeckRow deckName={deck} key={deck} />)}
+      <div className="decks-module">
+        <div className="title">Current Decks</div>
+        <div className="deck-list">
+          {
+            decks.map(deck => (
+              <DeckRow
+                deckName={deck}
+                key={deck}
+                onDeleteClick={this.onDeleteClick}
+                isConfirming={confirmDelete === deck} />))
+          }
+        </div>
       </div>
     )
   }
